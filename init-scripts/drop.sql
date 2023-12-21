7bdf2786632bc42c324458e6bb1155a6cc70641c
Sergio-Pedretti <sergiopedretti07@gmail.com>
2023-12-21 19:41:29 -0300

    feat(docker): add db scripts inside docker

---
+++ init-scripts/drop.sql
@@ -0,0 +1,6 @@
+drop table branas_store.items;
+drop table branas_store.product_dimensions;
+drop table branas_store.product;
+drop table branas_store.coupon;
+drop table branas_store.orders;
+drop schema branas_store;
